2ddc03634230de6ab737f39aad3d64922e98223a
mathieuHa <mathieu.hanotaux@gmail.com>
2016-11-10 11:54:10 +0100

    Regles pour utiliser Git

--- BiblioJava/src/Main.java
+++ BiblioJava/src/Main.java
@@ -3,6 +3,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		/*********************************
+		INSTRUCTIONS POUR TRAVAILLER AVEC GIT
+
+		QUAND VOUS BOSSER SUR LE CODE
+
+		ETAPE 1 : on récupère les modifs des autres (OBLIGATOIRE)
+		$git pull origin master
+
+		ETAPE 2 : on fait ses modifs on les tests
+		$git commit -a -m "les modifications que j'ai fait" <- le message est important genre ajout class
+		
+		ETAPE 3 : on envoi en ligne ses modifications
+		$git push origin master
+
+		**********************************/
 		System.out.println("initial commit");
 		System.out.println("Deuxième commit");
 	}
